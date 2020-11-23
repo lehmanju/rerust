4caf3d90b04c654f75e9c53a5756cd4de686b8db
Julius Lehmann <julius@devpi.de>
2020-11-23 13:05:30 +0100

    Remove multithreading in var

--- src/var.rs
+++ src/var.rs
@@ -2,74 +2,66 @@ use crate::signal::Signal;
 use futures::task::AtomicWaker;
 use std;
 use std::pin::Pin;
-use std::sync::Arc;
-use std::sync::RwLock;
-use std::task::Context;
+use std::{cell::RefCell, rc::Rc, task::Context};
 
 struct Inner<A>
 where
     A: Clone + PartialEq,
 {
     waker: AtomicWaker,
-    value: RwLock<A>,
-    transactions: RwLock<Vec<(u32, A)>>,
+    value: A,
+    transactions: Vec<(u32, A)>,
 }
-pub struct Var<A>(Arc<Inner<A>>)
+pub struct Var<A>(Rc<RefCell<Inner<A>>>)
 where
     A: Clone + PartialEq;
 
-unsafe impl<A> Send for Var<A> where A: Clone + PartialEq {}
-unsafe impl<A> Sync for Var<A> where A: Clone + PartialEq {}
-
 impl<A: Clone + PartialEq> Var<A> {
     pub fn new(value: A) -> Var<A> {
-        Var(Arc::new(Inner {
+        Var(Rc::new(RefCell::new(Inner {
             waker: AtomicWaker::new(),
-            value: RwLock::new(value),
-            transactions: RwLock::new(Vec::new()),
-        }))
+            value,
+            transactions: Vec::new(),
+        })))
     }
 
     pub fn set(&self, value: A) {
-        if *self.0.value.read().unwrap() != value {
-            *self.0.value.write().unwrap() = value;
-            self.0.waker.wake();
+        let inner = &mut *self.0.borrow_mut();
+        if inner.value != value {
+            inner.value = value;
+            inner.waker.wake();
         }
     }
 }
 
 impl<A: Clone + PartialEq> Signal for Var<A> {
     type Item = A;
-    fn poll(self: Pin<&mut Self>, cx: &mut Context, uuid: u32) -> Self::Item {
-        self.0.waker.register(cx.waker());
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context, uuid: u32) -> Self::Item {
+        let inner = &mut *self.0.borrow_mut();
+        inner.waker.register(cx.waker());
         // lookup transaction list for previous evaluation
-        let existing = self
-            .0
+        let existing = inner
             .transactions
-            .read()
-            .unwrap()
             .iter()
             .find(|(id, _)| *id == uuid)
             .map(|(_, val)| (*val).clone());
         // use cached value or else current value
-        existing.unwrap_or(self.0.value.read().unwrap().clone())
+        existing.unwrap_or(inner.value.clone())
     }
-    fn transaction_end(self: Pin<&mut Self>, uuid: u32) {
+    fn transaction_end(mut self: Pin<&mut Self>, uuid: u32) {
         // delete transaction with given uuid
-        self.0
-            .transactions
-            .write()
-            .unwrap()
-            .retain(|(u, _)| *u != uuid);
+        let inner = &mut *self.0.borrow_mut();
+        inner.transactions.retain(|(u, _)| *u != uuid);
     }
     fn value(&self) -> Self::Item {
-        self.0.value.read().unwrap().clone()
+        let inner = &*self.0.borrow();
+        inner.value.clone()
     }
 }
 
 impl<A: Clone + PartialEq> PartialEq for Var<A> {
     fn eq(&self, other: &Self) -> bool {
-        Arc::ptr_eq(&self.0, &other.0)
+        Rc::ptr_eq(&self.0, &other.0)
     }
 }
 
