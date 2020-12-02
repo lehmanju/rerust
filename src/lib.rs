570925db09d45aeca46c513e1dda7bc31a5cfa08
Julius Lehmann <julius@devpi.de>
2020-12-02 13:18:10 +0100

    Define simple API

--- src/lib.rs
+++ src/lib.rs
@@ -1,207 +1,88 @@
-//pub mod combinators;
-//pub mod fold;
-//pub mod signal;
-//pub mod var;
-
-mod test {
-
-    // fn fold1
-    // fn fold2
-    // fn fold3
-    // fn map
-
-    // (var1 -> fold1), (var1 -> fold2), (fold1 -> map), (fold2 -> map), (map -> fold3)
-
-    #[derive(Clone)]
-    pub struct ReactiveData {
-        fold1: u32,
-        fold2: String,
-        fold3: String,
-        var1: String,
-    }
-
-    pub struct ReactiveChange {
-        fold1: bool,
-        fold2: bool,
-        fold3: bool,
-        var1: bool,
-        map: bool,
-    }
-
-    pub struct Evaluation {
-        change: ReactiveChange,
-        data: ReactiveData,
-    }
-
-    pub struct Observers {
-        fold1: Vec<Box<dyn Fn(u32)>>,
-        fold2: Vec<Box<dyn Fn(String)>>,
-        fold3: Vec<Box<dyn Fn(String)>>,
-        var1: Vec<Box<dyn Fn(String)>>,
-    }
-
-    fn reduce(data: ReactiveData) -> Evaluation {
-        //let fold1_new = fold1(data.var1)
-        // cmp fold1_new fold1_old
-        //let fold2_new = fold2(data.var1)
-        // cmp
-        //let map_new = map(fold1_new, fold2_new)
-        // cmp
-        //let fold3_new = fold3(map_new)
-        //
-        //return evaluation
-        todo!()
-    }
-
-    fn call_observers(eval: Evaluation, observers: &Observers) {}
-
-    // loop: reduce, call_observers
-}
-
 mod compiler {
-    use petgraph::{Directed, Graph};
-    use std::{any::TypeId, rc::Rc};
 
-    trait Reactive {
-        type Item;
-        fn function(&self) -> &syn::Expr;
+    use std::{cell::RefCell, rc::Rc};
+
+    trait Reactive {}
+
+    struct Var {
+        initiale: &'static str,
+        reactives: Vec<Rc<RefCell<dyn Reactive>>>,
     }
 
-    trait Source {
-        type Item;
-        fn reactives_mut(&mut self) -> &mut Vec<Rc<dyn Reactive<Item = Self::Item>>>;
+    impl Reactive for Var {}
+
+    struct Map {
+        closure: &'static str,
+        reactives: Vec<Rc<RefCell<dyn Reactive>>>,
     }
 
-    trait Stateful {
-        type Item;
-        fn initial(&self) -> &syn::Expr;
+    impl Reactive for Map {}
+
+    struct Fold {
+        closure: &'static str,
+        reactives: Vec<Rc<RefCell<dyn Reactive>>>,
+        initiale: &'static str,
     }
 
-    struct Var<T>
-    where
-        T: Clone + PartialEq,
-    {
-        initial: (T, syn::Expr),
-        reactives: Vec<Rc<dyn Reactive<Item = T>>>,
+    impl Reactive for Fold {}
+
+    struct Filter {
+        closure: &'static str,
+        reactives: Vec<Rc<RefCell<dyn Reactive>>>,
     }
 
-    impl<T> Source for Var<T> where T: Clone + PartialEq {
-        type Item = T;
-        fn reactives_mut(&mut self) -> &mut Vec<Rc<dyn Reactive<Item = Self::Item>>> {
-        &mut self.reactives
-        }
+    impl Reactive for Filter {}
 
+    struct Choice {
+        reactives: Vec<Rc<RefCell<dyn Reactive>>>,
     }
 
-    impl<T> Stateful for Var<T> where T: Clone + PartialEq {
-        type Item = T;
-        fn initial(&self) -> &syn::Expr {
-            &self.initial.1
-        }
-    }
-
-    struct Fold<I, T>
-    where
-        I: Clone + PartialEq,
-        T: Clone + PartialEq,
-    {
-        initial: (T, syn::Expr),
-        function: (Box<dyn Fn(T, I) -> T>, syn::Expr),
-        reactives: Vec<Rc<dyn Reactive<Item = T>>>,
-    }
-
-    impl<I, T> Reactive for Fold<I, T>
-    where
-        T: Clone + PartialEq,
-        I: Clone + PartialEq,
-    {
-        type Item = I;
-        fn function(&self) -> &syn::Expr {
-            &self.function.1
-        }
-    }
-
-    impl<I, T> Source for Fold<I, T>
-    where
-        T: Clone + PartialEq,
-        I: Clone + PartialEq,
-    {
-        type Item = T;
-        fn reactives_mut(&mut self) -> &mut Vec<Rc<dyn Reactive<Item = Self::Item>>> {
-            &mut self.reactives
-        }
-    }
-
-    impl<I, T> Stateful for Fold<I, T>
-    where
-        T: Clone + PartialEq,
-        I: Clone + PartialEq,
-    {
-        type Item = T;
-        fn initial(&self) -> &syn::Expr {
-            &self.initial.1
-        }
-    }
-
-    trait SourceExt {
-        type Item: Clone + PartialEq;
-        fn fold<A, F>(
-            &mut self,
-            initial: (A, syn::Expr),
-            function: (F, syn::Expr),
-        ) -> &mut Fold<Self::Item, A>
-        where
-            A: Clone + PartialEq,
-            F: Fn(A, Self::Item) -> A;
-    }
-
-    impl<T> SourceExt for T
-    where
-        T: Source,
-        T::Item: Clone + PartialEq,
-    {
-        type Item = T::Item;
-        fn fold<A, F>(
-            &mut self,
-            initial: (A, syn::Expr),
-            function: (F, syn::Expr),
-        ) -> &mut Fold<Self::Item, A>
-        where
-            A: Clone + PartialEq,
-            F: Fn(A, Self::Item) -> A,
-        {
-            todo!()
-        }
-    }
+    impl Reactive for Choice {}
 
     macro_rules! var {
-        ($initial:expr, $t:ty) => {
-            struct Var<$t> {
-                initial: $t
-            }
+        ($init:expr) => {
+            Rc::new(RefCell::new(Var {
+                initiale: stringify!($init),
+                reactives: Vec::new(),
+            }))
         };
+    }
+
+    macro_rules! map {
+        ($($id:ident), +, $closure:block) => {
+            {
+                let rc = Rc::new(RefCell::new(Map { closure: stringify!($closure), reactives: Vec::new() }));
+                $($id.borrow_mut().reactives.push(rc.clone());)+
+                rc
+            }
+        }
+    }
+
+    macro_rules! fold {
+        ($($id:ident), +, $closure:block, $init:expr) => {
+            {
+                let rc = Rc::new(RefCell::new(Fold { closure: stringify!($closure), reactives: Vec::new(), initiale: stringify!($init)}));
+                $($id.borrow_mut().reactives.push(rc.clone());)+
+                rc
+            }
+        }
+    }
+
+    macro_rules! choice {
+        ($ida:ident, $idb:ident) => {{
+            let rc = Rc::new(RefCell::new(Choice {
+                reactives: Vec::new(),
+            }));
+            $ida.borrow_mut().reactives.push(rc.clone());
+            $idb.borrow_mut().reactives.push(rc.clone());
+            rc
+        }};
     }
 
     fn test() {
-        let var = Var {
-            initial: (
-                0u32,
-                syn::Expr::Lit(syn::ExprLit {
-                    attrs: vec![],
-                    lit: syn::Lit::Int(syn::LitInt::new("0u32", proc_macro2::Span::call_site())),
-                }),
-            ),
-            reactives: vec![],
-        };
-        /*var.fold((0u32, syn::Expr::Lit(syn::ExprLit {
-            attrs: vec![],
-            lit: syn::Lit::Int(syn::LitInt::new("0u32", proc_macro2::Span::call_site())),
-        }), (|))
-        let var = var!(initial); 
-        let fold = fold!(var, initial, fn); // generate struct and impls for fold,  add fold to mutable reference var, return mutable reference fold
-        let map = map!(var, fold, var2, fn); // generate struct and impl for map, consume all mutable references, return mutable reference to map
-
-        
-        */
+        let v = var!(0u32);
+        let map = map!(v, { v });
+        let fold = fold!(v, map, {}, 1i32);
+        let choice = choice!(v, map);
     }
 }
