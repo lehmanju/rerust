20d50b5b37d8ae25a2a0c4e5546d1fa9d480d2de
Julius Lehmann <julius@devpi.de>
2020-12-06 14:28:39 +0100

    Reorganize parser

--- examples/procmacrotest.rs
+++ examples/procmacrotest.rs
@@ -5,6 +5,7 @@ rerust! {
     let b = Var(0u32);
     let evt = Evt(i32);
     let c = (a,b).map(|ref mut a, mut b| -> u32 { a + b }) || (a,b).map(|a, b| -> u32 { a - b });
+    let evt_fold = evt.fold(String::new(), |mut string, evt| -> String { string });
 }
 
 fn main() {}
